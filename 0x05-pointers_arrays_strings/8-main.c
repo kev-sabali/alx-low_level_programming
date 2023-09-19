22710fafdd08b5d1bb19d952bc0478119b8462a1
sabali <140159033+kev-sabali@users.noreply.github.com>
2023-09-19 20:22:01 +0000

    update

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[] = {98 , 402 , -198 , 298 , -1024 };
+
+    print_array(array, 5);
+    return (0);
+}
