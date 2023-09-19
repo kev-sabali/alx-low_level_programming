795de5f8755147e666fe09395d2b664e22315d28
sabali <140159033+kev-sabali@users.noreply.github.com>
2023-09-19 16:48:37 +0000

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,14 @@
 /**
  * swap two values of integers
  *
- * @a: integer to swap
- * @b: second integer to swap
+ * @a: integer to swap_int
+ * @b: second integer to swap_int
  *
  * return : void
 */
 void swap_int(int *a, int *b)
 {
-    int temp_variable = *a;
-    *a = *b;
-    *b = temp_variable;
+int temp_variable = *a;
+*a = *b;
+*b = temp_variable;
 }
