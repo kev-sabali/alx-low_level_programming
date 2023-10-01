dbd968e60e09161f2eb20123bf5a220df448df04
sabali <140159033+kev-sabali@users.noreply.github.com>
2023-10-01 02:17:17 +0000

    update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- */
+*/
 int _putchar(char c)
 {
-    return (char*);
+  return (write(1, &c, 1));
 }
