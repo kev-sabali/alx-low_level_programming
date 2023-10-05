907f08219ff21256d521b468707eff90023726f2
sabali <140159033+kev-sabali@users.noreply.github.com>
2023-10-05 01:30:15 +0000

    update

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    return 0;
+    return strncpy(dest, src, n);
 }
