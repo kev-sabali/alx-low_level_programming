fa847d4dcd9261b7d5b78e1b34a4d9eafa17de4e
sabali <140159033+kev-sabali@users.noreply.github.com>
2023-10-10 02:33:09 +0000

    Structures,typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+struct dog my_poppy;
+my_poppy.name = "Spark" ;
+my_poppy.age = 5.6;
+my_poppy.owner = "Kay" ;
