12985de20a77249e91d049aa359ff8b876ea559f
sabali <140159033+kev-sabali@users.noreply.github.com>
2023-10-10 03:50:22 +0000

    Structures,typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,3 +14,10 @@ char *name;
 float age;
 char *owner;
 };
+/* Additional function prototypes */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
